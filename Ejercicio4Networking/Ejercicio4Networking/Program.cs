7cdf53203980cb028edc159cf6f99e5b76cbd727
Jaume97 <estevez.capon.jaume@gmail.com>
2020-02-13 14:49:22 +0200

    [13/02/2020]Ejercicio4Networking implements comands and connection to server[FAIL]

--- Ejercicio4Networking/Ejercicio4Networking/Program.cs
+++ Ejercicio4Networking/Ejercicio4Networking/Program.cs
@@ -13,27 +13,50 @@ namespace Ejercicio4Networking
 
     class Program
     {
-        static String Ip = "127.0.0.1", msg;
+        static String Ip = "127.0.0.1", msg,comand;
         static Random r = new Random();
         static Socket Server = null, client;
         static IPEndPoint ipServer, IpClient;
         static int port = 31416;
         static bool PortValid = false, condition = true;
-        static FileInfo f = new FileInfo(Environment.GetEnvironmentVariable("USERPROFILE") + "/Palabras.txt");
-        static StreamReader srPalabras = new StreamReader(f.FullName);//CERRAR ESTE SR
-        static String[] palabras = srPalabras.ReadToEnd().Split(',');
+        static FileInfo f = new FileInfo(Environment.GetEnvironmentVariable("USERPROFILE") + "/Palabras.txt"),
+            records= new FileInfo(Environment.GetEnvironmentVariable("USERPROFILE") + "/Records.txt");
+        static StreamWriter swPalabras = new StreamWriter(f.FullName),swRecords= new StreamWriter(records.FullName);
+        static StreamReader srPalabras = new StreamReader(f.FullName),srRecords= new StreamReader(records.FullName);//CERRAR ESTE SR
+        static String[] palabras = srPalabras.ReadToEnd().Split(','),ComandSplit;
         public static void FunctionThread(Object client)
         {
+            System.Diagnostics.Debug.WriteLine("Estoi dentro de functclient");
             Socket SocketClient = (Socket)client;
-            msg = palabras[r.Next(0, palabras.Length)];
             IpClient = (IPEndPoint)SocketClient.RemoteEndPoint;
 
             using (NetworkStream ns = new NetworkStream(SocketClient))
             using (StreamReader sr = new StreamReader(ns))
             using (StreamWriter sw = new StreamWriter(ns))
             {
-                sw.WriteLine(msg);
-                sw.Flush();
+                while (condition)
+                {
+                    comand = sr.ReadLine();
+                    ComandSplit = comand.Split(' ');
+                    if (ComandSplit[0]=="getword")
+                    {
+                        msg = palabras[r.Next(0, palabras.Length)];
+                        sw.WriteLine(msg);
+                        sw.Flush();
+                    }
+                    else if (ComandSplit[0] == "sendword")
+                    {
+                        swPalabras.Write(","+ComandSplit[1]);
+                    }
+                    else if (ComandSplit[0] == "getrecords")
+                    {
+                        sw.WriteLine(srRecords.ReadToEnd());
+                    }
+                    else if (ComandSplit[0]=="closeserver")
+                    {
+
+                    }
+                }
             }
         }
         static void Main(string[] args)
